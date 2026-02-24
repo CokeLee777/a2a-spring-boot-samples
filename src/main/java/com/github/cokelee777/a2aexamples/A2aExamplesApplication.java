3040bbf2383d2d1fca3c0342c41b3e1ecf1a4de9
CokeLee777 <akak640@kyonggi.ac.kr>
2026-02-24 22:34:47 +0900

    init: Project initialize

---
+++ src/main/java/com/github/cokelee777/a2aexamples/A2aExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.github.cokelee777.a2aexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2aExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A2aExamplesApplication.class, args);
+    }
+
+}
