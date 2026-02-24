f84fbbf86474b769d9b2cbf57510189ea9349570
이채민 <chaemin.lee01@lotte.net>
2026-02-25 08:25:45 +0900

    chore: Separate into a2a client and server modules

--- a2a-client/src/main/java/com/github/cokelee777/a2aclient/A2aClientApplication.java
+++ a2a-client/src/main/java/com/github/cokelee777/a2aclient/A2aClientApplication.java
@@ -1,13 +1,12 @@
-package com.github.cokelee777.a2aexamples;
+package com.github.cokelee777.a2aclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class A2aExamplesApplication {
+public class A2aClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(A2aExamplesApplication.class, args);
+        SpringApplication.run(A2aClientApplication.class, args);
     }
-
 }
---
+++ a2a-server/src/main/java/com/github/cokelee777/a2aserver/A2aServerApplication.java
@@ -0,0 +1,12 @@
+package com.github.cokelee777.a2aserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2aServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A2aServerApplication.class, args);
+    }
+}
